8f5993754913b4af3e8ad8ec60c58a54d230467a
AmmoMax <steelratmax@gmail.com>
2020-05-28 21:27:32 +0300

    minor fix for atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -22,7 +22,7 @@ int	ft_atoi(char *str)
 	sign = 1;
 	res = 0;
 	if (!str[i])
-		return (NULL);
+		return (0);
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || \
 			str[i] == '\f' || str[i] == '\v' || str[i] == '\n')
 		i++;
