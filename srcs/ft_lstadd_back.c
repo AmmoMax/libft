368673fad10bf9b836be556fbcd029964c637f3c
AmmoMax <steelratmax@gmail.com>
2020-09-21 23:23:59 +0300

    fix errors in ft_lstadd_back func

--- srcs/ft_lstadd_back.c
+++ srcs/ft_lstadd_back.c
@@ -6,18 +6,22 @@
 /*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 21:16:41 by amayor            #+#    #+#             */
-/*   Updated: 2020/09/02 20:48:29 by amayor           ###   ########.fr       */
+/*   Updated: 2020/09/02 22:54:39 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#define ERROR 1
+#define OK 0
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+int			ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *start;
+	t_list	*start;
 
 	if (!lst)
-		return ;
+		return (ERROR);
+	if (!new)
+		return (ERROR);
 	start = *lst;
 	if (start)
 	{
@@ -25,7 +29,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 			start = start->next;
 		start->next = new;
 		new->next = NULL;
+		return (OK);
 	}
 	else
 		*lst = new;
+	return (OK);
 }
--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 19:50:39 by amayor            #+#    #+#             */
-/*   Updated: 2020/08/31 22:10:42 by amayor           ###   ########.fr       */
+/*   Updated: 2020/09/02 22:55:01 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_list **lst, t_list *new);
+int					ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
